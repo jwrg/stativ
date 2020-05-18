5f269bfb8acc945ae8e1728efff79599d7f0afb5
Jim Galloway <jwrg@uvic.ca>
2020-05-17 18:53:58 -0700

    Improve performance somewhat
    There were just too many calls to head and tail (there
    are still too many but this is a WIP).  This script
    needs little else as much more text tends to not fit
    into the dwm status bar.

--- stativ.sh
+++ stativ.sh
@@ -3,51 +3,45 @@
 BATTERY_PERCENTAGE=/sys/class/power_supply/BAT1/capacity
 BATTERY_ALARM=/sys/class/power_supply/BAT1/alarm
 
-cpu_0_prev_line=($(head -n2 /proc/stat | tail -1))
-cpu_0_prev_reading="${cpu_0_prev_line[@]:1}"
+cpu_prev_line=($(head -n5 /proc/stat | tail -4))
+cpu_0_prev_reading="${cpu_prev_line[@]:1:10}"
 cpu_0_prev_reading=$((${cpu_0_prev_reading// /+}))
-cpu_1_prev_line=($(head -n3 /proc/stat | tail -1))
-cpu_1_prev_reading="${cpu_1_prev_line[@]:1}"
+cpu_1_prev_reading="${cpu_1_prev_line[@]:12:10}"
 cpu_1_prev_reading=$((${cpu_1_prev_reading// /+}))
-cpu_2_prev_line=($(head -n4 /proc/stat | tail -1))
-cpu_2_prev_reading="${cpu_2_prev_line[@]:1}"
+cpu_2_prev_reading="${cpu_2_prev_line[@]:23:10}"
 cpu_2_prev_reading=$((${cpu_2_prev_reading// /+}))
-cpu_3_prev_line=($(head -n5 /proc/stat | tail -1))
-cpu_3_prev_reading="${cpu_3_prev_line[@]:1}"
+cpu_3_prev_reading="${cpu_3_prev_line[@]:34:10}"
 cpu_3_prev_reading=$((${cpu_3_prev_reading// /+}))
 disk_prev_line=($(cat /proc/diskstats | tail -9 | head -1))
 sleep 1
 
 while true; do
-  cpu_0_line=($(head -n2 /proc/stat | tail -1))
-  cpu_0_reading="${cpu_0_line[@]:1}"
+  cpu_line=($(head -n5 /proc/stat | tail -4))
+  cpu_0_reading="${cpu_line[@]:1:10}"
   cpu_0_reading=$((${cpu_0_reading// /+}))
   cpu_0_delta=$((cpu_0_reading - cpu_0_prev_reading))
-  cpu_0_idle=$((cpu_0_line[4] - cpu_0_prev_line[4]))
+  cpu_0_idle=$((cpu_line[4] - cpu_prev_line[4]))
   cpu_0_used=$((cpu_0_delta - cpu_0_idle))
   cpu_0_usage=$((100 * cpu_0_used / cpu_0_delta)) 
 
-  cpu_1_line=($(head -n3 /proc/stat | tail -1))
-  cpu_1_reading="${cpu_1_line[@]:1}"
+  cpu_1_reading="${cpu_line[@]:12:10}"
   cpu_1_reading=$((${cpu_1_reading// /+}))
   cpu_1_delta=$((cpu_1_reading - cpu_1_prev_reading))
-  cpu_1_idle=$((cpu_1_line[4] - cpu_1_prev_line[4]))
+  cpu_1_idle=$((cpu_line[15] - cpu_prev_line[15]))
   cpu_1_used=$((cpu_1_delta - cpu_1_idle))
   cpu_1_usage=$((100 * cpu_1_used / cpu_1_delta)) 
 
-  cpu_2_line=($(head -n4 /proc/stat | tail -1))
-  cpu_2_reading="${cpu_2_line[@]:1}"
+  cpu_2_reading="${cpu_line[@]:23:10}"
   cpu_2_reading=$((${cpu_2_reading// /+}))
   cpu_2_delta=$((cpu_2_reading - cpu_2_prev_reading))
-  cpu_2_idle=$((cpu_2_line[4] - cpu_2_prev_line[4]))
+  cpu_2_idle=$((cpu_line[26] - cpu_prev_line[26]))
   cpu_2_used=$((cpu_2_delta - cpu_2_idle))
   cpu_2_usage=$((100 * cpu_2_used / cpu_2_delta)) 
 
-  cpu_3_line=($(head -n5 /proc/stat | tail -1))
-  cpu_3_reading="${cpu_3_line[@]:1}"
+  cpu_3_reading="${cpu_line[@]:34:10}"
   cpu_3_reading=$((${cpu_3_reading// /+}))
   cpu_3_delta=$((cpu_3_reading - cpu_3_prev_reading))
-  cpu_3_idle=$((cpu_3_line[4] - cpu_3_prev_line[4]))
+  cpu_3_idle=$((cpu_line[37] - cpu_prev_line[37]))
   cpu_3_used=$((cpu_3_delta - cpu_3_idle))
   cpu_3_usage=$((100 * cpu_3_used / cpu_3_delta)) 
 
@@ -68,14 +62,14 @@ while true; do
   uptime_minutes=$(((uptime - (uptime_days * 86400) - (uptime_hours * 3600) ) / 60))
 
   status=""
-  status+=`printf "%3d" $cpu_0_usage `
-  status+="%"
-  status+=`printf "%3d" $cpu_1_usage `
-  status+="%"
-  status+=`printf "%3d" $cpu_2_usage `
-  status+="%"
-  status+=`printf "%3d" $cpu_3_usage `
-  status+="% | "
+  status+=`printf "%2d" $cpu_0_usage `
+  status+="% "
+  status+=`printf "%2d" $cpu_1_usage `
+  status+="% "
+  status+=`printf "%2d" $cpu_2_usage `
+  status+="% "
+  status+=`printf "%2d" $cpu_3_usage `
+  status+="% |"
   status+=`printf "%3d" $mem_free `
   status+="% Free | R:"
   status+=`printf "%3d" $disk_reads`
@@ -89,13 +83,10 @@ while true; do
 
   xsetroot -name "${status}"
 
-  cpu_0_prev_line=("${cpu_0_line[@]}")
+  cpu_prev_line=("${cpu_line[@]}")
   cpu_0_prev_reading=$cpu_0_reading
-  cpu_1_prev_line=("${cpu_1_line[@]}")
   cpu_1_prev_reading=$cpu_1_reading
-  cpu_2_prev_line=("${cpu_2_line[@]}")
   cpu_2_prev_reading=$cpu_2_reading
-  cpu_3_prev_line=("${cpu_3_line[@]}")
   cpu_3_prev_reading=$cpu_3_reading
   disk_prev_line=("${disk_line[@]}")
 
